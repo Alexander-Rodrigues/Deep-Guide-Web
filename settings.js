b094c480498ffb1d549386da7d79f0bfd9889eee
Zerfalex <Zerfalex@gmail.com>
2021-05-24 11:24:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,12 +6,17 @@ window.onbeforeunload = function (e) {
 	settings.volume = document.querySelector('#volume').value;
 	settings.deviceType = document.querySelector('#option1').checked ? 'earbuds' : 'headphones'
 	settings.deviceName = document.querySelector('#devicename').value;
-	settings.mode = mode;
+	saveSettings();
 };
 
+//set volume
 document.querySelector('#volume').value = settings.volume;
+
+//set device type
 if (settings.deviceType == 'earbuds') {
 	document.querySelector('#option1').click();
 }
 else document.querySelector('#option2').click();
+
+//set device name
 document.querySelector('#devicename').value = settings.deviceName;
