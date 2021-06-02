16f5e1e66d2ee0a65d019b169ac65791fdf5fda1
Zerfalex <Zerfalex@gmail.com>
2021-06-02 11:50:56 +0100

    Add 'other' option to headphone type
    -Headphone type can be: 'earbuds', 'headphones', 'other', 'default'
    -default if not picked in settings
    -other by default if visited settings

--- settings.js
+++ settings.js
@@ -4,20 +4,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 window.onbeforeunload = function (e) {
 	settings.volume = document.querySelector('#volume').value;
-	settings.deviceType = document.querySelector('#option1').checked ? 'earbuds' : 'headphones'
+	settings.deviceType = document.querySelector('.btn-radio.active').firstElementChild.value
+	console.log(settings);
 	settings.deviceName = document.querySelector('#devicename').value;
 	saveSettings();
-	saveStats();
 };
+
+//Setting up page from local storage
 
 //set volume
 document.querySelector('#volume').value = settings.volume;
 
-//set device type
+/* //set device type
 if (settings.deviceType == 'earbuds') {
 	document.querySelector('#option1').click();
 }
 else document.querySelector('#option2').click();
+ */
+
+//set device type
+switch (settings.deviceType) {
+	case 'earbuds':
+		document.querySelector('#option1').click();
+		break;
+	case 'headphones':
+		document.querySelector('#option2').click();
+		break;
+	case 'other':
+		document.querySelector('#option3').click();
+		break;
+}
 
 //set device name
 document.querySelector('#devicename').value = settings.deviceName;
